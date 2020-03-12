a03bfebcc95959cb390fb554f5e8a61a3e499035
kkraune <kristian@ymail.com>
2020-03-12 15:50:13 +0100

    feed cloud docs to search

--- Gemfile
+++ Gemfile
@@ -1,12 +1,11 @@
+# Copyright Verizon Media. All rights reserved
+
 source "https://rubygems.org"
+ruby '>=2.5'
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-gem "github-pages", group: :jekyll_plugins
-
-group :jekyll_plugins do
-   gem "jekyll-feed", "~> 0.6"
-end
+gem "github-pages"
 
 # Get the html-proofer to work
 gem 'rake'
---
+++ _plugins/vespa_index_generator.rb
@@ -0,0 +1,40 @@
+# Copyright Verizon Media. All rights reserved
+
+require 'json'
+require 'nokogiri'
+
+module Jekyll
+
+    class VespaIndexGenerator < Jekyll::Generator
+        priority :lowest
+
+        def generate(site)
+            namespace = site.config["search"]["namespace"]
+            operations = []
+            site.pages.each do |page|
+                if page.data["index"] == true
+                    operations.push({
+                        :fields => {
+                            :path => page.url,
+                            :namespace => namespace,
+                            :title => page.data["title"],
+                            :content => extract_text(page)
+                        }
+                    })
+                end
+            end
+
+            json = JSON.pretty_generate(operations)
+            File.open(namespace + "_index.json", "w") { |f| f.write(json) }
+        end
+
+        def extract_text(page)
+            doc = Nokogiri::HTML(page.content)
+            doc.search('th,td').each{ |e| e.after "\n" }
+            content = doc.xpath("//text()").to_s
+            page_text = content.gsub("\r"," ").gsub("\n"," ")
+        end
+
+    end
+
+end
